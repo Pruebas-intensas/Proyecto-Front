264f275c25d042d06051c89ceb36ef3ac7497f0f
Rodrigo Tallar <89488066+rtallarr@users.noreply.github.com>
2023-11-13 21:12:20 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  url_back: 'backend-psw.azurewebsites.net',
+  url_back: 'https://backend-psw.azurewebsites.net',
   url_front: 'https://pruebas-intensas.github.io'
 }
