78860defa971056f0293ef1bd2c8b80710cca085
Rodrigo Tallar <89488066+rtallarr@users.noreply.github.com>
2023-11-13 21:07:36 -0300

    hotfix: Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  url_back: 'http://localhost:3000',
-  url_front: 'http://localhost:4200'
+  url_back: 'backend-psw.azurewebsites.net',
+  url_front: 'https://pruebas-intensas.github.io'
 }
